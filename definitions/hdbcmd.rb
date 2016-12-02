2673391e4de8f123d23f7f0aa959faa420d6827e
Wesley Creager <wesley.creager@bluemedora.com>
2016-12-02 16:23:58 -0500

    removed commented out sections

--- definitions/hdbcmd.rb
+++ definitions/hdbcmd.rb
@@ -15,72 +15,13 @@ define :hdbcmd, :exe => "", :bin_dir => "", :bin_file_url => "" do
       action :create
       backup false
     end
-=begin
-    if params[:bin_file_url].start_with?("http")
-    execute "Get Hana binary package" do
-      cwd "#{node['install']['tempdir']}"
-      command "wget --progress=dot:giga #{params[:bin_file_url]} -O SAP_HANA_PACKAGE.SAR"
-    end
-=end
-    #elsif params[:bin_file_url].start_with?("file")
+
     remote_file "#{node['install']['tempdir']}/SAP_HANA_PACKAGE.SAR" do
       source "#{node['install']['files']['hanadb']}"
       action :create
       backup false
     end
-=begin
-  elsif
 
-     unless $already_done
-       directory "#{node['install']['productionmountpoint1']}" do
-         action :create
-         recursive true 
-       end
-
-       mount "#{node['install']['productionmountpoint1']}" do
-         device "#{node['install']['productiondevice1']}"
-         not_if "mountpoint -q #{node['install']['productionmountpoint1']}"
-         fstype "nfs"
-         action :mount
-       end
-  
-       directory "#{node['install']['productionmountpoint2']}" do
-         action :create
-         recursive true 
-       end
-       mount "#{node['install']['productionmountpoint2']}" do
-         device "#{node['install']['productiondevice2']}"
-         not_if "mountpoint -q #{node['install']['productionmountpoint2']}"
-         fstype "nfs"
-         action :mount
-       end
-  
-       directory "#{node['install']['productionmountpoint3']}" do
-         action :create
-         recursive true 
-       end
-       mount "#{node['install']['productionmountpoint3']}" do
-         device "#{node['install']['productiondevice3']}"
-         not_if "mountpoint -q #{node['install']['productionmountpoint3']}"
-         fstype "nfs"
-         action :mount
-       end
-     end
-
-    execute "Get     #{params[:bin_file_url]}" do
-      cwd "#{node['install']['tempdir']}"
-      command "cp #{params[:bin_file_url]} SAP_HANA_PACKAGE.SAR"
-    end
-
-  end
-=end
-  #remote_file would fit both variants, but seems to be very slow compared to wget and cp
-  #remote_file "Get SAP_HANA_PACKAGE.SAR file" do
-  #    source "#{params[:bin_file_url]}"
-  #    path "#{node['install']['tempdir']}/SAP_HANA_PACKAGE.SAR"
-  #    backup false
-  #end
-  
   execute "Extract #{params[:bin_file_url]}" do
     cwd "#{node['install']['tempdir']}"
     command "chmod +x SAPCAR && ./SAPCAR -xvf SAP_HANA_PACKAGE.SAR"
@@ -103,5 +44,4 @@ define :hdbcmd, :exe => "", :bin_dir => "", :bin_file_url => "" do
     rmtempdir "clean up /monsoon/tmp directory"
   end
 
-  $already_done = true
 end
